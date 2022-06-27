5996c6a74896c838ae7a9f58948904018abd3f63
Natdanai Thanwanichakoon <natdadai.t@ku.th>
2022-06-27 12:16:28 +0700

    initial commit : add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,11 @@
+package myapp;
+
+public class Cat {
+    public static void Voice(){
+        System.out.println("Meow!");
+    }
+
+    public static void main(String[] args) {
+        Cat.Voice();
+    }
+}
