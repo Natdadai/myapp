675ea0f7c763e620b75a975f133752d5a81fef3a
Natdanai Thanwanichakoon <natdadai.t@ku.th>
2022-06-27 12:21:07 +0700

    Update Cat and add Dog

---
+++ src/myapp/Dog.java
@@ -0,0 +1,10 @@
+package myapp;
+
+public class Dog {
+    public static void Voice(){
+        System.out.println("Hong!");
+    }
+    public static void main(String[] args) {
+        Dog.Voice();
+    }
+}
