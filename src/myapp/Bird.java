690da50d367328bb507dcce5ac8b8f419b6dcb8f
Natdanai Thanwanichakoon <natdadai.t@ku.th>
2022-06-27 12:42:10 +0700

    Add Bird

---
+++ src/myapp/Bird.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class Bird {
+
+    public static void fly(){
+        System.out.println("fly");
+    }
+
+    public static void main(String[] args) {
+        Bird.fly();
+    }
+}
